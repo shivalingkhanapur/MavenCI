2359a397a7d81fd7a34096b0b8be23a22c0dc4d4
Shivaling Khanapur <sw378951@gmail.com>
2025-11-06 17:56:47 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven Project!");
+    }
+
+    public String message() {
+        return "Hello from App Class!";
+    }
+}
